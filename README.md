d58681fa8700953d91cf7850afe2edadc7f0d199
Nitin Raturi <raturi002@gmail.com>
2020-01-10 15:51:37 +0530

    add: multiple environments config

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # django-structure
+Pre configured django project for making development easy
+
+## Usage
+- git clone https://github.com/nitinraturi/django-structure.git
+- pip install -r requirements.txt
+- create a file .env where manage.py is located and set your environment variables. Have a look same file: .env-sample
+- That's all, start developing...
+
+## Features
+- Django version 2.2 (LTS).
+- Manage multiple environments
--- conf/settings.py
+++ conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q37(4#a3xja_d*wr&jpa(ld4w+%=t@%qh&%p%!b!y!f@l74j+a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
